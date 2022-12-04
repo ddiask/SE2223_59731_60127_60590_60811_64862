7e126507b8ae9cc91f603b565f6dc287f711d8b3
Afonso Nunes <113634775+AfonsoNunes64862@users.noreply.github.com>
2022-12-04 00:16:31 +0000

    Added comments to the LoginFromDemo class.

--- ganttproject/src/net/sourceforge/ganttproject/email/LoginFromDemo.java
+++ ganttproject/src/net/sourceforge/ganttproject/email/LoginFromDemo.java
@@ -1,23 +1,27 @@
-//import required classes and packages
 package net.sourceforge.ganttproject.email;
 
+import javax.swing.*;
 import net.sourceforge.ganttproject.gui.UIFacade;
 
-import javax.swing.*;
-
-
-
-
+/**
+ * Class used to create the JFrame that supports the e-mail graphic interface
+ * and a CreateLoginForm form that will contain the mail recipients and the path to the file.
+ */
 public class LoginFromDemo
 {
-
+    /**
+     * The loginMail method that is responsible to create the form containing the resources e-mail, t
+     * he file path and the graphic interface facade.
+     * @param mail -> The EmailInfo object with the e-mail information: Has the list of recipients and the file to be exported.
+     * @param uiFacade -> The facade used to create the graphic interface
+     */
     public void loginMail(EmailInfo mail, UIFacade uiFacade)
     {
         try
         {
             CreateLoginForm form = new CreateLoginForm(mail, uiFacade);
             form.pack();
-            form.setVisible(true);  //make form visible to the user
+            form.setVisible(true);
 
         }
         catch(Exception e)
